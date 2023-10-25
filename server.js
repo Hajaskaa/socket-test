70bd2d7b1cfb777bae342804c87464e4bc45312b
Nar Zoltán Tarkan <tarkan.zoltan.nar@gmail.com>
2023-10-25 22:52:44 +0200

    10/25 deploy test

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const server = createServer(app);
 const io = new Server(server);
 
 app.get("/", (req, res) => {
-  const url = new URL("./index.html", import.meta.url).pathname;
-  console.log(url);
-  res.sendFile(new URL("./index.html", import.meta.url).pathname);
+  //const url = new URL("./index.html", import.meta.url).pathname;
+  res.sendFile("/*");
 });
 
 io.on("connection", (socket) => {
